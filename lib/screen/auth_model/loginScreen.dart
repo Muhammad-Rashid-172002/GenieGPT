96940a7375e2341901adc597b480b2b576784ccf
Muhammad Rashid <muhammadrashid172002@gmail.com>
2025-06-17 10:52:33 -0700

    1st commit

---
+++ lib/screen/auth_model/loginScreen.dart
@@ -0,0 +1,43 @@
+import 'package:chatbot/screen/homescreen.dart';
+import 'package:flutter/material.dart';
+
+class Loginscreen extends StatefulWidget {
+  const Loginscreen({super.key});
+
+  @override
+  State<Loginscreen> createState() => _LoginscreenState();
+}
+
+class _LoginscreenState extends State<Loginscreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Login Screen'),
+        backgroundColor: Colors.blue,
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            const Text(
+              'Login Screen',
+              style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
+            ),
+            const SizedBox(height: 20),
+            ElevatedButton(
+              onPressed: () {
+                // Navigate to Home Screen
+                Navigator.pushReplacement(
+                  context,
+                  MaterialPageRoute(builder: (context) => const Homescreen()),
+                );
+              },
+              child: const Text('Go to Home Screen'),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
---
+++ lib/screen/splashScreen/SplashScreen.dart
@@ -0,0 +1,67 @@
+import 'package:chatbot/screen/auth_model/loginScreen.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_spinkit/flutter_spinkit.dart';
+import 'package:lottie/lottie.dart';
+
+class Splashscreen extends StatefulWidget {
+  const Splashscreen({super.key});
+
+  @override
+  State<Splashscreen> createState() => _SplashscreenState();
+}
+
+class _SplashscreenState extends State<Splashscreen> {
+  @override
+  void initState() {
+    super.initState();
+
+    Future.delayed(const Duration(seconds: 6), () {
+      Navigator.pushReplacement(
+        context,
+        MaterialPageRoute(builder: (context) => const Loginscreen()),
+      );
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.blue,
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            // Lottie Animation
+            Lottie.asset(
+              'assets/animation assets/Animation - 1748579152630.json',
+              width: 200,
+              height: 200,
+              fit: BoxFit.contain,
+            ),
+
+            const SizedBox(height: 20),
+
+            // App Title
+            const Text(
+              'Welcome\nTo ChatBot App!!',
+              textAlign: TextAlign.center,
+              style: TextStyle(
+                fontWeight: FontWeight.bold,
+                color: Colors.white,
+                fontSize: 24,
+                height: 1.4,
+              ),
+            ),
+
+            const SizedBox(height: 30),
+
+            // Circular Loading Indicator
+            const SpinKitFadingCircle(color: Colors.white, size: 60.0),
+
+            const SizedBox(height: 30),
+          ],
+        ),
+      ),
+    );
+  }
+}
